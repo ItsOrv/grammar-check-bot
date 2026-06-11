8922e61335d77a79358427700a6af20c11c00a95
ItsOrv <poriya.saw@gmail.com>
2026-06-11 13:17:44 +0330

    per user cooldown so people dont get spammed

---
+++ app/services/cooldown.py
@@ -0,0 +1,16 @@
+import time
+
+
+class Cooldown:
+    """Per (chat, user) cooldown so one user isn't corrected repeatedly."""
+
+    def __init__(self, seconds: int):
+        self.seconds = seconds
+        self._last: dict[tuple[int, int], float] = {}
+
+    def is_active(self, chat_id: int, user_id: int) -> bool:
+        last = self._last.get((chat_id, user_id))
+        return last is not None and (time.monotonic() - last) < self.seconds
+
+    def mark(self, chat_id: int, user_id: int) -> None:
+        self._last[(chat_id, user_id)] = time.monotonic()
